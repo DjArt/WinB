67c8b708d97e388ce27ddc1ea691fde7c7fd5bbe
DjArt <art.letter.mail@gmail.com>
2019-10-15 03:59:29 +0700

    apply-ffu command added

--- modules/DISM.psm1
+++ modules/DISM.psm1
@@ -1,24 +1,29 @@
 function Unmount-Image([string]$dirname)
 {
-  Invoke-Expression "dism /Unmount-Image /MountDir:$dirname /Commit"
+    Invoke-Expression "dism /Unmount-Image /MountDir:$dirname /Commit"
 }
 
 function Discard-Image([string]$dirname)
 {
-  Invoke-Expression "dism /Unmount-Image /MountDir:$dirname /Discard"
+    Invoke-Expression "dism /Unmount-Image /MountDir:$dirname /Discard"
 }
 
 function Add-DriversToImage([string]$imagedir, [string]$driversdir)
 {
-  Invoke-Expression "dism /Image:$imagedir /Add-Driver /Driver:$driversdir /Recurse"
+    Invoke-Expression "dism /Image:$imagedir /Add-Driver /Driver:$driversdir /Recurse"
 }
 
 function Add-PackagesToImage([string]$imagedir, [string]$packagesdir)
 {
-  Invoke-Expression "dism /Image:$imagedir /Add-Package /PackagePath:$packagesdir"
+    Invoke-Expression "dism /Image:$imagedir /Add-Package /PackagePath:$packagesdir"
 }
 
 function Mount-Image([string]$imagefile, [string]$dirname, [int]$index = 1)
 {
-  Invoke-Expression "dism /Mount-Image /ImageFile:$imagefile /Index:$index /MountDir:$dirname"
+    Invoke-Expression "dism /Mount-Image /ImageFile:`"$imagefile`" /Index:$index /MountDir:`"$dirname`""
+}
+
+function Apply-FFU([string]$imageFile, [string]$applyDrive)
+{
+    Invoke-Expression "dism /Apply-FFU /ImageFile=`"$imageFile`" /ApplyDrive:$applyDrive"
 }
